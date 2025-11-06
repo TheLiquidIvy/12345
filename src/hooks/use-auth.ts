6e57edd3ddf1080343380b42052ea215c2905681
theliquidivy <theliquidivy@gmail.com>
2025-11-06 17:52:42 +0000

    auth

--- src/hooks/use-auth.ts
+++ src/hooks/use-auth.ts
@@ -3,8 +3,9 @@ import { useAuthStore } from '@/store/auth-store';
 
 export const useAuth = () => {
   const user = useAuthStore((state) => state.user);
+  const isAuthenticated = useAuthStore((state) => state.isAuthenticated);
   const setUser = useAuthStore((state) => state.setUser);
   const logout = useAuthStore((state) => state.logout);
 
-  return { user, setUser, logout };
+  return { user, isAuthenticated, setUser, logout };
 };
