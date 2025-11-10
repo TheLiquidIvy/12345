cc3fead14902895198c6d0354f737abb13b91588
theliquidivy <theliquidivy@gmail.com>
2025-11-10 11:15:00 +0000

    edits

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,14 +3,13 @@ import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getStorage, ref, uploadBytes, getDownloadURL } from 'firebase/storage';
 
-// TODO: Replace with your firebase credentials
 const firebaseConfig = {
-  apiKey: 'YOUR_API_KEY',
-  authDomain: 'YOUR_AUTH_DOMAIN',
-  projectId: 'YOUR_PROJECT_ID',
-  storageBucket: 'YOUR_STORAGE_BUCKET',
-  messagingSenderId: 'YOUR_MESSAGING_SENDER_ID',
-  appId: 'YOUR_APP_ID',
+  apiKey: "AIzaSyCzvIIyWgCEUAVa9JQxNtk3R_VyJ6bgt9o",
+  authDomain: "pixelpfirebase.firebaseapp.com",
+  projectId: "pixelpfirebase",
+  storageBucket: "pixelpfirebase.appspot.com",
+  messagingSenderId: "853848520433",
+  appId: "1:853848520433:web:997c77560b73e693f51b50"
 };
 
 const app = initializeApp(firebaseConfig);
